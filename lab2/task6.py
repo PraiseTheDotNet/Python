3a72ebc21dd1d1ea60524541dec2a4ae0752164d
PraiseTheDotNet <alan.walter.vyatsu@gmail.com>
2020-09-24 20:44:45 +0300

    Добавлены задания 6 и 7 для лаб 2

---
+++ lab2/task6.py
@@ -0,0 +1,51 @@
+import re
+
+
+def parse_with_regex(input: str):
+    return re.findall(r'[а-яА-Яa-zA-Z]+', input)
+
+
+def parse_without_regex(input: str):
+    word_list = []
+    word = ''
+    for s in input:
+        if s.isalpha():
+            word += s
+        elif len(word):
+            word_list.append(word)
+            word = ''
+    if len(word):
+        word_list.append(word)
+    return word_list
+
+
+def group_words(word_list):
+    word_list.sort()
+    count = 0
+    words = []
+    for i in range(len(word_list)):
+        if i < len(word_list) - 1 and word_list[i] == word_list[i + 1]:
+            count += 1
+        else:
+            if count == 0:
+                words.append((word_list[i], 1))
+            else:
+                words.append((word_list[i], count + 1))
+                count = 0
+    return words
+
+def print_data(word_list, name):
+    print('\nСписок слов', name, ':')
+    word_list = group_words(word_list)
+    word_list.sort(key=lambda tup: len(tup[0]), reverse=True)
+    for (word, count) in word_list:
+        if count > 1:
+            print(word, '(', count, ')', end=', ')
+        else:
+            print(word, end=', ')
+
+
+if __name__ == '__main__':
+    input = "Ехал грека через реку, видит грека — в реке рак. Сунул грека руку в реку, рак за руку греку — цап!  цап цап цап"
+    print_data(parse_with_regex(input), 'с регексом')
+    print_data(parse_without_regex(input), 'без регекса')
---
+++ lab2/task7.py
@@ -0,0 +1,62 @@
+import re
+from datetime import date
+
+
+def format_with_regex(input: str):
+    result = input
+    result = re.sub(r'(?P<space>[-\(\{\[])', r' \g<space> ', result)
+    result = re.sub(r'\s+', r' ', result)
+    result = re.sub(r'(\s(?=[.,!?:;\)\]\}]))|((?<=[\(\{\[])\s+)', r'', result)
+    return result
+
+
+def format_without_regex(input: str):
+    word_list = []
+    word = ''
+    space = False
+    last = ''
+    for s in input:
+        if not s.isspace():
+            if s == '-':
+                if len(word):
+                    word_list.append(word)
+                    word = ''
+                word_list.append(s)
+                space = False
+            if s.isalnum() and (not space or last in ['(', '[', '{']):
+                word += s
+                space = False
+            elif s.isalnum():
+                if len(word):
+                    word_list.append(word)
+                word = s
+                space = False
+            elif s in ['(', '[', '{'] :
+                if len(word):
+                    word_list.append(word)
+                word = s
+                space = False
+            elif s in ['.', ',', '!', '?', ';', ':', ')', '[', ']', '{', '}']:
+                # if last == '.' and s == '.' or last.isalnum():
+                word += s
+                # else:
+                #     if len(word):
+                #         word_list.append(word)
+                #     word_list.append(s)
+                #     space = False
+                #     word = ''
+            last = s
+        else:
+            space = True
+    if len(word):
+        word_list.append(word)
+    return ' '.join(word_list)
+
+if __name__ == '__main__':
+    input = "Ехал ли грека ? Ехал  грека  через реку , видит грека-в реке рак . Сунул ( грека ) руку в [ реку   ], рак {   за} руку греку - (цап)!  цап цап цап . . . "
+    print('Входные данные:')
+    print(input)
+    print('Регекс:')
+    print(format_with_regex(input))
+    print('Не регекс:')
+    print(format_without_regex(input))
